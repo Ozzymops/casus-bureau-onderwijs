43767d2d1cef830017b6070aec135cbe103424c7
Justin Muris <1736302muris@zuyd.nl>
2018-10-08 14:52:25 +0200

    db connection string opzet

--- BureauOnderwijs/BureauOnderwijs/Models/Database.cs
+++ BureauOnderwijs/BureauOnderwijs/Models/Database.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,26 @@ namespace BureauOnderwijs.Models
     public class Database
     {
         // Zie Wesley code
-        private string connectionString = "";
+
+        protected SqlConnection conn;
+        /// <summary>
+        /// This method is used to setup the connection string which is used to connect to the database.
+        /// Initialise this after log-in.
+        /// </summary>
+        /// <returns>connection string</returns>
+        public SqlConnection Connect()
+        {
+            string connString = @"Data Source=moodlestudiepad.database.windows.net;" +
+                                 "Initial Catalog=MoodleStudiepad;" + "User id=moodleadmin;" +
+                                 "Password=#studiepad01;"; // edit to new Azure db !!!
+            conn = new SqlConnection(connString);
+            return conn;
+        }
+
 
         #region Get
         // get functies: lees uit een table
+        // create things via queries and send them back to the requester.
         #endregion
 
         #region Add
