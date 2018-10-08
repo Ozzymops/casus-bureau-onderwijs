30f8f4676e7c7c12ea693ce755baed8e1f039d64
Justin Muris <1736302muris@zuyd.nl>
2018-10-08 14:44:59 +0200

    db opzet

---
+++ BureauOnderwijs/BureauOnderwijs/Models/Database.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BureauOnderwijs.Models
+{
+    public class Database
+    {
+        // Zie Wesley code
+        private string connectionString = "";
+
+        #region Get
+        // get functies: lees uit een table
+        #endregion
+
+        #region Add
+        // add functies: voeg toe aan een table
+        #endregion
+
+        #region Update
+        // update functies: werk table bij
+        #endregion
+
+        #region Delete
+        // delete functies: verwijder data uit een table
+        // DROP NOOIT EEN TABLE! Hiermee verlies je ALLE data!
+        #endregion
+    }
+}
