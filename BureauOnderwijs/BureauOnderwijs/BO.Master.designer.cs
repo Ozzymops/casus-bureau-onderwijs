3721db3055c1fe3e2a22b200ef5f209f1bb8b0ad
Justin Muris <1736302muris@zuyd.nl>
2018-10-09 12:01:09 +0200

    Navbar

--- BureauOnderwijs/BureauOnderwijs/BO.Master.designer.cs
+++ BureauOnderwijs/BureauOnderwijs/BO.Master.designer.cs
@@ -40,13 +40,13 @@ namespace BureauOnderwijs {
         protected global::System.Web.UI.WebControls.Label TitleLabel;
         
         /// <summary>
-        /// Menu1 control.
+        /// NavMenu control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu Menu1;
+        protected global::System.Web.UI.WebControls.Menu NavMenu;
         
         /// <summary>
         /// HeadLabel control.
