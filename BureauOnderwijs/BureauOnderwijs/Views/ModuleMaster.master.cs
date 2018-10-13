a79a76c295b93fbfbabac0274e41a6932776a7ae
Justin Muris <1736302muris@zuyd.nl>
2018-10-13 12:37:00 +0200

    nested masterpage

--- BureauOnderwijs/BureauOnderwijs/Views/ModuleMaster.master.cs
+++ BureauOnderwijs/BureauOnderwijs/Views/ModuleMaster.master.cs
@@ -7,12 +7,16 @@ using System.Web.UI.WebControls;
 
 namespace BureauOnderwijs.Views
 {
-    public partial class Modules : System.Web.UI.Page
+    public partial class ModuleMaster : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
+        protected void addModuleButton_Click(object sender, EventArgs e)
+        {
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('fucking aids');", true);
+        }
     }
 }
--- BureauOnderwijs/BureauOnderwijs/Views/Modules.aspx
+++
@@ -1,3 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/NestedModule.master" AutoEventWireup="true" CodeBehind="Modules.aspx.cs" Inherits="BureauOnderwijs.Views.TestNest" %>
-<asp:Content ID="NestedContent" ContentPlaceHolderID="NestedContentHolder" runat="server">
-</asp:Content>
