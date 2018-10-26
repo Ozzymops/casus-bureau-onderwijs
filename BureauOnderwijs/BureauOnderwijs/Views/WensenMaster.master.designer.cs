bf248f4ce2f8cae88a64e81b56f4d199e1a19c66
Youri Peusen <youri-peusen@hotmail.com>
2018-10-26 16:10:49 +0200

    Usecase Exporteren van wensenlijst naar Excel is gemaakt en werkt lokaal. De opslaan-locatie van het bestand moet nog dynamisch worden gemaakt.

--- BureauOnderwijs/BureauOnderwijs/Views/WensenMaster.master.designer.cs
+++ BureauOnderwijs/BureauOnderwijs/Views/WensenMaster.master.designer.cs
@@ -49,6 +49,15 @@ namespace BureauOnderwijs.Views {
         protected global::System.Web.UI.WebControls.Button deleteWishButton;
         
         /// <summary>
+        /// ExportWishesButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ExportWishesButton;
+        
+        /// <summary>
         /// NestedPlaceholder1 control.
         /// </summary>
         /// <remarks>
