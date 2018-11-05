6cbf9a6d418dc48bf05a564847080468bb6feafc
Youri Peusen <youri-peusen@hotmail.com>
2018-11-05 17:17:15 +0100

    Administrator-button wordt niet getoont aan niet-admins in de navbar

--- BureauOnderwijs/BureauOnderwijs/Views/MasterPage.Master.designer.cs
+++ BureauOnderwijs/BureauOnderwijs/Views/MasterPage.Master.designer.cs
@@ -31,6 +31,15 @@ namespace BureauOnderwijs.Views {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// AdminButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor AdminButton;
+        
+        /// <summary>
         /// welcomeLabel control.
         /// </summary>
         /// <remarks>
